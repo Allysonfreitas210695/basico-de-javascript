43723c52cd9e554bb258380d237600b23a58b0dd
Allysonfreitas210695 <alisonfr83@gmail.com>
2020-09-13 00:06:57 -0300

    Javacript Treino

---
+++ Javascript/Array/Propriedades/Length/index.js
@@ -0,0 +1,10 @@
+//criaçao estatica do array
+const array = [1,2,3,4,5,6,7,8,9,10];
+
+//contagem de elementos dentro do array com a Propriedade Length
+const Cont = array.length
+
+//imprimir tamanho do vetor
+console.log(Cont);
+
+
