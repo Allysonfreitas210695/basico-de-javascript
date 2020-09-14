4b91d095fb4f6d08f10540510322ad5d915902a9
Allysonfreitas210695 <alisonfr83@gmail.com>
2020-09-14 00:05:39 -0300

    ForEach

---
+++ Javascript/Array/metados/ForEach/index.js
@@ -0,0 +1,9 @@
+/**
+ * O método forEach() executa uma dada função em cada elemento de um array.
+ */
+
+const vetor = [1,2,3,4,6,7,8,9,10];
+
+vetor.forEach(function(value){
+    console.log(value);
+})
