060c03d71676511787cb6e0daa47215245cd7d25
Allysonfreitas210695 <alisonfr83@gmail.com>
2020-09-14 00:32:39 -0300

    Join

---
+++ Javascript/Array/metados/Join/index.js
@@ -0,0 +1,10 @@
+//O método join() junta todos os elementos de uma array (ou um array-like object) em uma string e retorna esta string.
+
+const v = ["allyson", "bruno", "de", "freitas","fernandes"];
+
+console.log(v.join(" "));
+
+
+const numero = [1,2,3,4,5];
+
+console.log(numero.join( ));
