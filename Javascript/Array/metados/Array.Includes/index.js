9e90452386a3c9185f27d84c20c06347022284b3
Allysonfreitas210695 <alisonfr83@gmail.com>
2020-09-14 00:16:29 -0300

    Includes

---
+++ Javascript/Array/metados/Array.Includes/index.js
@@ -0,0 +1,11 @@
+//olha se tem um condicao dentro do array se tiver retonar true senao retona false
+
+const vetor = [
+    1,2,3,2,5,6,2,8,
+]
+
+
+const take = vetor.includes(2);
+
+
+console.log(take.valueOf());
