6d370df55419c86e5b4df82a7c3ea67182e1971a
Allysonfreitas210695 <alisonfr83@gmail.com>
2020-09-16 20:10:44 -0300

    Metado Pop

---
+++ Javascript/Array/metados/Pop/index.js
@@ -0,0 +1,12 @@
+//O método pop() remove o último elemento de um array e retorna aquele elemento.
+
+const vetor = ["arroz", "batata", "farinha","macaxeira"];
+
+const takeElemen = vetor.pop();
+
+//mostra elemento que foi tirado da ultima posição
+console.log(takeElemen);
+
+//mostra o vetor sem a ultima posiçao
+console.log(vetor);
+
