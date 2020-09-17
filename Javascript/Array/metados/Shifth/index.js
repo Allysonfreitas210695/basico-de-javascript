af39dc9ff33ea6e3461daef7d70cd442236bb580
Allysonfreitas210695 <alisonfr83@gmail.com>
2020-09-17 20:01:41 -0300

    Shift

---
+++ Javascript/Array/metados/Shifth/index.js
@@ -0,0 +1,7 @@
+//O método shift()remove o primeiro elemento de um array e retorna esse elemento. Este método muda o tamanho do array. 
+
+const vetor = [1,2,3,4,5,6,7,8,9,10];
+
+const TakePosicao = vetor.shift();
+
+console.log(TakePosicao);
