21f7608f6c209c5c333d8e874f3a9008c8336a0c
Allysonfreitas210695 <alisonfr83@gmail.com>
2020-09-17 19:54:55 -0300

    Reverse

---
+++ Javascript/Array/metados/Reverse/index.js
@@ -0,0 +1,7 @@
+//O método reverse() inverte os itens de um array. O primeiro elemento do array se torna o último e o último torna-se o primeiro.
+
+const vetor = [1,2,3,4,5,6,7,8,9,9,0,13];
+
+const inverso = vetor.reverse();
+
+console.log(inverso);
